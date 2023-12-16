e8937748ffdf8d15d2b4dae1008c68e641858341
Shahabas Mohammed <shahabasg1@gmail.com>
2023-12-16 22:10:37 +0530

    path 5

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'frontend/dist' folder
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 const port = 5000;
