c16ad6891a12297b84f7bd4db55176a60a635ea0
Shahabas Mohammed <shahabasg1@gmail.com>
2023-12-16 19:17:00 +0530

    env changed

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 PORT =5000
-NODE_ENV=development
+NODE_ENV=production
 MONGO_URI=mongodb://shahabas123:1234@ac-j0buk9g-shard-00-00.lb49czb.mongodb.net:27017,ac-j0buk9g-shard-00-01.lb49czb.mongodb.net:27017,ac-j0buk9g-shard-00-02.lb49czb.mongodb.net:27017/?ssl=true&replicaSet=atlas-132d51-shard-0&authSource=admin&retryWrites=true&w=majority
 JWT_SECRET=1234
 FROMMAIL='codept100@gmail.com'
