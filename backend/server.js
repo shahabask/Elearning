942666cd1de41ee26517520c896fe1ee809ce188
Shahabas Mohammed <shahabasg1@gmail.com>
2023-12-17 20:33:26 +0530

    image folder added

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+ 
 // Serve static files from the 'frontend/dist' folder
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
