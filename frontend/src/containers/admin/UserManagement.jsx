b1ce3820f59b28dc6bdc03895eb4fdbb6ab65d5a
Shahabas Mohammed <shahabasg1@gmail.com>
2024-01-22 09:12:50 +0530

    check2

--- frontend/src/containers/admin/UserManagement.jsx
+++ frontend/src/containers/admin/UserManagement.jsx
@@ -85,8 +85,8 @@ export default function UserManagement() {
     const fetchData = async () => {
       try {
         const res = await axiosInstance.get('/loadUsers');
-        const rowsWithIndex = res.data.users.map((user, index) => ({ ...user, index: index + 1,subscriptionMode:user.subscription.mode.subscriptionMode }));
-       
+        const rowsWithIndex = res.data.users.map((user, index) => ({ ...user, index: index + 1,subscriptionMode:user.subscription?.mode?.subscriptionMode }));
+         console.log(rowsWithIndex[0])
         setRows(rowsWithIndex);
        
         
